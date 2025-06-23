cbb0594cd0355687c1fabc70ee51588257d0b2c7
Vishwapatel-india <vishwapate1810@gmail.com>
2025-06-23 17:57:13 +0530

    if else

---
+++ Assignment/Module=1/assignment_1/greater_less_no.py
@@ -0,0 +1,12 @@
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+
+if a>b:
+    print(f'{A} is greater than {B}')
+
+elif b>a:
+    print(f'{B} is greater than {A}')
+
+else:
+    print(' a is equal to b')
+
