899c5b7db79d725f5a71e4196a14e39af13650bf
Vishwapatel-india <vishwapate1810@gmail.com>
2025-06-23 11:49:16 +0530

    que-1 to 5

---
+++ Assignment/Module=1/assignment_1/comments_variable.py
@@ -0,0 +1,13 @@
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+#print("Sum of above numbers=",a+b)
+print("Choose any of the above options:", "\n 1=addition", "\n 2=subtraction", "\n 3=mulitiply", "\n 4=divide")
+
+
+print(f"A:{a} and B:{b}")
+print("ADD:",a+b)
+print("SUB:",a-b)
+print("MUL:",a*b)
+print("DIV:",a/b)
+
+
