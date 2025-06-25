c9a430e4d718354ca8ede3e223b0519c3b9f3272
Vishwapatel-india <vishwapate1810@gmail.com>
2025-06-25 09:24:33 +0530

    Prime number

--- Assignment/Module=1/assignment_1/prime_number.py
+++ Assignment/Module=1/assignment_1/prime_number.py
@@ -3,9 +3,15 @@ a=int(input("Enter a number:"))
 if a<=1:
     print("The number is not a prime number")
 
+elif a==2:
+    print("prime number")
+
 else:
     for i in range(2,a):
-        if a % i ==0:
+        if a % i==0:
             print("Not a prime number")
             break
+
+        else:
+            print("Prime number")
     
