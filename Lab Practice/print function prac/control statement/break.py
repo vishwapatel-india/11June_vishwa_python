dc1d6b9e52bfc95d9c0d9b048000d41b78bc9ce8
Vishwapatel-india <vishwapate1810@gmail.com>
2025-06-25 09:59:47 +0530

    control

---
+++ Lab Practice/print function prac/control statement/break.py
@@ -0,0 +1,5 @@
+for i in range(1,11):
+    print(i)
+    #if i==5:
+    if i>=5:
+        break #in between if its true it breaks
---
+++ Lab Practice/print function prac/control statement/pass.py
@@ -0,0 +1,8 @@
+"""a=32
+b=45
+
+if a<b:
+    pass #where u need to avoid any block or comment"""
+    
+for i in range(1,11):
+    pass
