cdc08f1188b8ad0910bf9b57b2beb5a443b31e81
Vishwapatel-india <vishwapate1810@gmail.com>
2025-06-27 10:55:09 +0530

    hard

---
+++ Lab Practice/print function prac/collection types/list.py
@@ -0,0 +1,36 @@
+data=['python','php','java','C++']
+#print(data)
+
+"""print(data[0])
+print(len(data))
+membership 
+if 'php' in data:
+    print('Yes.....')
+else: 
+    print('Noo')
+"""
+
+"""for i in data: #dont give range
+    print(i)
+
+print(data.index('python'))"""
+
+"""for i in data:
+    print(f"{data.index(i)} - {i}")
+"""
+#update the method
+"""data[2]='android'
+print(data)
+"""
+#list methods
+print(data)
+data.append('react')
+data.insert(2,'CSS')
+data.remove('java')
+data.pop() #last value removed
+data.pop(1)
+data.clear() #value cleared
+del data
+data.sort()
+print(data)
+
