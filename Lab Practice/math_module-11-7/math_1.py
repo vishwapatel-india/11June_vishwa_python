f7ab2994b9b5d7a3990855fa3ab8c64c087ce8b2
Vishwapatel-india <vishwapate1810@gmail.com>
2025-07-11 10:06:41 +0530

    s

---
+++ Lab Practice/math_module-11-7/math_1.py
@@ -0,0 +1,20 @@
+import math
+
+"""print(math.pi)
+print(math.e)
+"""
+"""x=math.ceil(79.85)
+print(x)
+"""
+"""x=math.floor(45.45)
+x=math.factorial(4)
+x=math.sqrt(25)
+x=math.power(5,4)
+x=math.sin(10)
+"""
+x=math.log10(10)
+
+
+
+
+print(x)
---
+++ Lab Practice/math_module-11-7/platfoem.py
@@ -0,0 +1,6 @@
+import platform
+
+print(platform.architecture())
+print(platform.processor())
+print(platform.python_version())
+print(platform.machine())
