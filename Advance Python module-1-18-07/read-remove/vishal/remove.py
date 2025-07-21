394bd917c28c897b6b58bb48e1def5a2c98d0685
Vishwapatel-india <vishwapate1810@gmail.com>
2025-07-21 10:37:31 +0530

    done

---
+++ Advance Python module-1-18-07/read-remove/vishal/remove.py
@@ -0,0 +1,14 @@
+"""import os
+
+os.chdir('vishal')
+open('remove.txt','x')
+
+os.remove('remove')
+print('Removed')
+
+os.removedirs('vishal')
+print("The deletion task is successful!!!")"""
+
+import shutil
+
+shutil.rmtree('Rajkot')
