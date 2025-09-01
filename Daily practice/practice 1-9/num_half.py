8ae8b4b49132597109901e11666a3bec1a41ee77
Vishwapatel-india <vishwapate1810@gmail.com>
2025-09-01 18:17:16 +0530

    mew

---
+++ Daily practice/practice 1-9/num_half.py
@@ -0,0 +1,6 @@
+n=5
+
+for i in range(1,n+1):   #here i=1,2,3,4,5
+    for j in range(1,i+1):  #here j= 1,2,3,4,5 
+        print(j, end=" ")
+    print()
---
+++ Daily practice/practice 1-9/rev_half_tri.py
@@ -0,0 +1,8 @@
+"""n=5
+
+for i in range(n,0,-1):
+    print('*' * i)"""
+
+n = 5
+for i in range(n, 0, -1):
+    print("*" * i)
